1bf0aff530e5449c3755c0855356349dcd1b65c4
Playingtygre <kymiehsu123@gmail.com>
2018-04-12 22:54:45 -0700

    Regit

--- University/University/Program.cs
+++ University/University/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Student_Enrollment
+namespace University
 {
     public class Program
     {
