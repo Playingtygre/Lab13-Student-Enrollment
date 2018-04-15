db49ce8dc151e023104bb3fe7088c574254b96ab
Playingtygre <kymiehsu123@gmail.com>
2018-04-14 17:19:43 -0700

    Attemping somthing else

---
+++ CollegeRandom/CollegeRandom/Models/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CollegeRandom.Models
+{
+    public class Course
+    {
+        public int ID { get; set; }
+
+        [StringLength(30, MinimumLength = 1)]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Level { get; set; }
+
+        public string Instructor { get; set; }
+    }
+}
